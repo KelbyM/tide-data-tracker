295d91e285a36bf8b2beb73fd9bb3f329b4792f1
Kelby Mannigel <kelby.mannigel@gmail.com>
2024-12-26 17:08:08 -0800

    Created the DayManager class.

---
+++ src/main/java/DayManager.java
@@ -0,0 +1,127 @@
+import java.util.ArrayList;
+
+public class DayManager {
+
+    // INSTANCE VARIABLES
+
+    private static DayManager dayManager;
+
+    /** The file path to the tide data file. */
+    private String tideDataFilePath;
+
+    /** The file path to the moon data file. */
+    private String moonDataFilePath;
+
+    /** A year of tide and moon data. */
+    private ArrayList<Day> yearData;
+
+    // CONSTRUCTORS
+
+    private DayManager(String tideDataFilePath, String moonDataFilePath) {
+        this.tideDataFilePath = tideDataFilePath;
+        this.moonDataFilePath = moonDataFilePath;
+        // populate yearData
+        parseYear();
+    }
+
+    // SINGLETON PATTERN METHODS
+
+    /**
+     * Initializes the dayManager instance variable.
+     * @param tideDataFilePath The file path to the tide data file.
+     * @param moonDataFilePath The file path to the moon data file.
+     */
+    public static void createInstance(String tideDataFilePath, String moonDataFilePath) {
+        if(dayManager == null) {
+            dayManager = new DayManager(tideDataFilePath, moonDataFilePath);
+        }
+        else {
+            System.out.println("Error: createInstance has already been called.");
+        }
+    }
+
+    /**
+     * Returns the dayManager instance variable.
+     * @return The dayManager variable.
+     */
+    public static DayManager getInstance() {
+        if(dayManager == null) {
+            System.out.println("Error: createInstance has not been called yet.");
+            return null;
+        }
+        return dayManager;
+    }
+
+    // ASSESSORS/MUTATORS
+    public static DayManager getDayManager() {
+        return dayManager;
+    }
+
+    public static void setDayManager(DayManager dayManager) {
+        DayManager.dayManager = dayManager;
+    }
+
+    public String getTideDataFilePath() {
+        return tideDataFilePath;
+    }
+
+    public void setTideDataFilePath(String tideDataFilePath) {
+        this.tideDataFilePath = tideDataFilePath;
+    }
+
+    public String getMoonDataFilePath() {
+        return moonDataFilePath;
+    }
+
+    public void setMoonDataFilePath(String moonDataFilePath) {
+        this.moonDataFilePath = moonDataFilePath;
+    }
+
+    public ArrayList<Day> getYearData() {
+        return yearData;
+    }
+
+    public void setYearData(ArrayList<Day> yearData) {
+        this.yearData = yearData;
+    }
+
+    // OTHER METHODS
+
+    /**
+     * Creates and returns a Day object with tide and moon data.
+     * @param tideDataProcessor Reads tide data.
+     * @param moonDataProcessor Reads moon data.
+     * @return A Day object containing tide and moon data.
+     */
+    private Day parseDay(TideDataProcessor tideDataProcessor, MoonDataProcessor moonDataProcessor) {
+        // get the tide data for the day
+        ArrayList<TideData> tides = tideDataProcessor.getNextTideData();
+        // get the moon data for the day
+        MoonData moonData = moonDataProcessor.getNextMoonData();
+        // return the day object
+        return new Day(tides.getFirst().getTime().toLocalDate(), tides, moonData);
+    }
+
+    /**
+     * Populates the ArrayList with Day objects containing data from the data files.
+     */
+    private void parseYear() {
+        yearData = new ArrayList<>();
+        // create the data processors
+        TideDataProcessor tideDataProcessor = new TideDataFileReader(tideDataFilePath);
+        MoonDataProcessor moonDataProcessor = new MoonDataFileReader(moonDataFilePath);
+        // parse all data from the files
+        while(moonDataProcessor.hasNextMoonData()) {
+            yearData.add(parseDay(tideDataProcessor, moonDataProcessor));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "DayManager{" +
+                "TideDataFilePath='" + tideDataFilePath + '\'' +
+                ", MoonDataFilePath='" + moonDataFilePath + '\'' +
+                ", yearData=" + yearData +
+                '}';
+    }
+}
