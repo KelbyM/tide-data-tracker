efcb75cebd0d820e0a4b4c733c9e19d48495d807
Kelby Mannigel <kelby.mannigel@gmail.com>
2025-01-06 10:59:09 -0800

    Created the LoadDataViewController class. The Load Data and Return button works.

---
+++ src/main/java/org/kelbymannigel/tidedatatracker/LoadDataViewController.java
@@ -0,0 +1,45 @@
+package org.kelbymannigel.tidedatatracker;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class LoadDataViewController {
+
+    // CONTROL ELEMENTS
+
+    @FXML
+    protected Button returnButton,
+                     loadDataButton;
+
+    @FXML
+    protected TextField tideDataFilePathTextField,
+                        moonDataFilePathTextField;
+
+    // ACTIONS
+
+    @FXML
+    protected void onReturnButtonPressed(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("MainView.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    protected void onLoadDataButtonPressed() {
+        DayManager manager = DayManager.getInstance();
+        manager.setTideDataFilePath(tideDataFilePathTextField.getText());
+        manager.setMoonDataFilePath(moonDataFilePathTextField.getText());
+        manager.parseYear();
+    }
+}
--- src/main/java/org/kelbymannigel/tidedatatracker/MainViewController.java
+++ src/main/java/org/kelbymannigel/tidedatatracker/MainViewController.java
@@ -1,4 +1,58 @@
 package org.kelbymannigel.tidedatatracker;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class MainViewController {
+
+    private DayManager manager;
+
+    // CONTROL ELEMENTS
+
+    @FXML
+    protected DatePicker startDatePicker,
+                         endDatePicker;
+
+    @FXML
+    protected TextField startTimeTextField,
+                        endTimeTextField,
+                        minHeightTextField,
+                        maxHeightTextField,
+                        minMoonIlluminationTextField,
+                        maxMoonIlluminationTextField;
+
+    @FXML
+    protected RadioButton lowTideRadioButton,
+                          highTideRadioButton;
+
+    @FXML
+    protected Button searchButton,
+                     loadDataButton;
+
+    // ACTIONS
+
+    @FXML
+    protected void onLoadDataButtonPressed(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("LoadDataView.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    protected void onSearchButtonPressed() {
+
+    }
 }
