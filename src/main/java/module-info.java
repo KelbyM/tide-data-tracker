cf97d06f30e18e2a4a6596ff5f6ecb0446998985
Kelby Mannigel <kelby.mannigel@gmail.com>
2025-02-12 20:42:39 -0800

    Add persistence and hibernate dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module org.kelbymannigel.tidedatatracker {
+    requires java.naming;
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.hibernate.orm.core;
+    requires jakarta.persistence;
 
-
-    opens org.kelbymannigel.tidedatatracker to javafx.fxml;
+    opens org.kelbymannigel.tidedatatracker to org.hibernate.orm.core, javafx.fxml;
     exports org.kelbymannigel.tidedatatracker;
 }
